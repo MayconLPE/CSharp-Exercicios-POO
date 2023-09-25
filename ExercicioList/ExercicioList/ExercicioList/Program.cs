266e346c3797edb8355d86d2583e18bf0d23a6c6
MayconL27 <mayconleandro2008@gmail.com>
2023-09-25 08:51:42 -0300

    ExercicioList

---
+++ ExercicioList/ExercicioList/ExercicioList/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+
+        list.Add("Maycon");
+        list.Add("Leandro");
+        list.Add("Maria");
+        list.Insert(1, "João");
+
+        foreach (string item in list)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("List count: " + list.Count);
+
+        string s1 = list.Find(x => x[0] == 'M');
+        Console.WriteLine("First 'M': " + s1);
+    }
+}
