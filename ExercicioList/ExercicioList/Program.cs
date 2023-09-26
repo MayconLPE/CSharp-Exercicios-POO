57fafc71f53bb5170100083b496ba0b8e8e6dc41
MayconL27 <mayconleandro2008@gmail.com>
2023-09-26 08:38:27 -0300

    ExercicioList Atualizado

---
+++ ExercicioList/ExercicioList/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+
+        list.Add("Maycon");
+        list.Add("Leandro");
+        list.Add("Maria");
+        list.Insert(1, "João");
+
+        foreach (string item in list)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("List count: " + list.Count);
+
+        string s1 = list.Find(x => x[0] == 'M'); // pegar primeiro nome que começam com M
+        Console.WriteLine("First 'M': " + s1);
+
+        string s2 = list.FindLast(x => x[0] ==  'M'); // pegar ultimo nome que começam com M
+        Console.WriteLine("Last 'M': " + s2);
+
+        int pos1 = list.FindIndex(x => x[0] == 'M');
+        Console.WriteLine("First position 'A': " + pos1);
+
+        int pos2 = list.FindLastIndex(x => x[0] == 'M');
+        Console.WriteLine("First position 'A': " + pos2);
+
+        List<string> list2 = list.FindAll(x => x.Length == 5);
+        Console.WriteLine("---------------");
+        foreach (String obj in list2)
+        {
+            Console.WriteLine(obj);
+        }
+
+    }
+}
