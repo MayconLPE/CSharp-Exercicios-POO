7a2260c12ab3c80b599f7cd7704c66083515cf7f
MayconL27 <mayconleandro2008@gmail.com>
2023-10-02 07:53:52 -0300

    CondicionalTernaria

---
+++ ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {      
+        Console.Write("Digite o preço: ");
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+        Console.WriteLine(desconto);
+    }
+}
