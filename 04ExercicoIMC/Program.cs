fc5c199ce2717a2940fc30cd5f9103e8fc35043d
MayconL27 <mayconleandro2008@gmail.com>
2024-03-27 12:43:21 -0300

    Exercio 04 IMC

---
+++ 04ExercicoIMC/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        1 - Na classe pessoa adicionar os atributos: peso e altura
+        2 - Criar um método para retornar o IMC, será um método double com o cálculo: peso / (altura * altura)
+        3 - Criar um método para retornar um string dependendo do IMC:
+        < 18.5 = Abaixo do peso.
+        < 25 = peso normal.
+        < 30 = Acima do peso.
+        < 35 = obesidade 1.
+        < 40 = Obesidade 2.
+        >= 40 = Obesidade 3.
+        4 - Criar um método de mensagem para exibir o cálculo e a situação. */
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
